e6a0affe6c3340ddd94b921c3ac40e61b75cb1c6
Guanchu Wang <hegsns@gmail.com>
2024-08-01 22:06:51 -0700

    upload bench

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# rare_disease_benchmark
+# Assessing and Enhancing Large Language Models in Rare Disease Question-answering
+
+This is the official codebase of paper _Assessing and Enhancing Large Language Models in Rare Disease Question-answering_.
+
+## Dataset Overview
+
+
+
+## Dependency
+```
+numpy
+scikit-learn
+scipy
+torch
+accelerate==0.32.1
+transformers==4.42.4
+datasets==2.20.0
+ipdb
+tqdm
+```
+
+## Quick Exploration on the Benchmark
+
+Run LLMs w/o RAG on the ResDis-QA dataset:
+```bash
+cd zero-shot-bench
+bash ./scripts/run_exp.sh
+```
+
+Run RAG with ReCOP corpus on the ResDis-QA dataset:
+```bash
+cd meta-data-bench
+bash ./scripts/run_exp.sh
+```
+
+
+
--- zero-shot-bench/scripts/run_exp.sh
+++ zero-shot-bench/scripts/run_exp.sh
@@ -1,54 +1,5 @@
-# CUDA_VISIBLE_DEVICES=1 python phi_inference.py
-# CUDA_VISIBLE_DEVICES=1 python Qwen2_inference.py
-# CUDA_VISIBLE_DEVICES=1 python llama_inference.py
-# CUDA_VISIBLE_DEVICES=1 python gemma_inference.py
-# CUDA_VISIBLE_DEVICES=1 python mistral_inference.py
-# CUDA_VISIBLE_DEVICES=1 python phi_inference_corpus.py           --k 5
-# CUDA_VISIBLE_DEVICES=1 python Qwen2_inference_corpus.py         --k 5
-# CUDA_VISIBLE_DEVICES=1 python llama_inference_corpus.py         --k 5
-# CUDA_VISIBLE_DEVICES=1 python gemma_inference_corpus.py         --k 5
-# CUDA_VISIBLE_DEVICES=1 python mistral_inference_corpus.py       --k 5
-
-# CUDA_VISIBLE_DEVICES=1 python mistral_inference_corpus.py     --k 1
-# CUDA_VISIBLE_DEVICES=1 python mistral_inference_corpus.py     --k 2
-# CUDA_VISIBLE_DEVICES=1 python mistral_inference_corpus.py     --k 3
-# CUDA_VISIBLE_DEVICES=1 python mistral_inference_corpus.py     --k 4
-# CUDA_VISIBLE_DEVICES=1 python mistral_inference_corpus.py     --k 6
-# CUDA_VISIBLE_DEVICES=1 python mistral_inference_corpus.py     --k 7
-
-# CUDA_VISIBLE_DEVICES=1 python phi_inference_corpus.py     --k 1
-# CUDA_VISIBLE_DEVICES=1 python Qwen2_inference_corpus.py   --k 1
-# CUDA_VISIBLE_DEVICES=1 python llama_inference_corpus.py   --k 1
-# CUDA_VISIBLE_DEVICES=1 python gemma_inference_corpus.py   --k 1
-#
-# CUDA_VISIBLE_DEVICES=1 python phi_inference_corpus.py     --k 2
-# CUDA_VISIBLE_DEVICES=1 python Qwen2_inference_corpus.py   --k 2
-# CUDA_VISIBLE_DEVICES=1 python llama_inference_corpus.py   --k 2
-# CUDA_VISIBLE_DEVICES=1 python gemma_inference_corpus.py   --k 2
-#
-# CUDA_VISIBLE_DEVICES=1 python phi_inference_corpus.py     --k 3
-# CUDA_VISIBLE_DEVICES=1 python Qwen2_inference_corpus.py   --k 3
-# CUDA_VISIBLE_DEVICES=1 python llama_inference_corpus.py   --k 3
-# CUDA_VISIBLE_DEVICES=1 python gemma_inference_corpus.py   --k 3
-#
-# CUDA_VISIBLE_DEVICES=1 python phi_inference_corpus.py     --k 4
-# CUDA_VISIBLE_DEVICES=1 python Qwen2_inference_corpus.py   --k 4
-# CUDA_VISIBLE_DEVICES=1 python llama_inference_corpus.py   --k 4
-# CUDA_VISIBLE_DEVICES=1 python gemma_inference_corpus.py   --k 4
-#
-# CUDA_VISIBLE_DEVICES=1 python phi_inference_corpus.py     --k 6
-# CUDA_VISIBLE_DEVICES=1 python Qwen2_inference_corpus.py   --k 6
-# CUDA_VISIBLE_DEVICES=1 python llama_inference_corpus.py   --k 6
-# CUDA_VISIBLE_DEVICES=1 python gemma_inference_corpus.py   --k 6
-#
-# CUDA_VISIBLE_DEVICES=1 python phi_inference_corpus.py     --k 7
-# CUDA_VISIBLE_DEVICES=1 python Qwen2_inference_corpus.py   --k 7
-# CUDA_VISIBLE_DEVICES=1 python llama_inference_corpus.py   --k 7
-# CUDA_VISIBLE_DEVICES=1 python gemma_inference_corpus.py   --k 7
-
-# CUDA_VISIBLE_DEVICES=1 python mistral_explanantion.py   --k 7
-# CUDA_VISIBLE_DEVICES=3 python gemma_inference_corpus_v3.py   --k 7
-# CUDA_VISIBLE_DEVICES=0 python Qwen2_inference_corpus_v3.py   --k 7
-CUDA_VISIBLE_DEVICES=0 python llama_inference_corpus_v3.py   --k 7
-# CUDA_VISIBLE_DEVICES=3 python phi_inference_corpus_v3.py   --k 7
-
+CUDA_VISIBLE_DEVICES=0 python phi_inference.py
+CUDA_VISIBLE_DEVICES=0 python Qwen2_inference.py
+CUDA_VISIBLE_DEVICES=0 python llama_inference.py
+CUDA_VISIBLE_DEVICES=0 python gemma_inference.py
+CUDA_VISIBLE_DEVICES=0 python mistral_inference.py
