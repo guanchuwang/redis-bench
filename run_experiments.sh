30ab5c2a637866a6be8387c0e48d25c133283b4a
frankran <jr151@rice.edu>
2024-08-02 12:02:16 -0500

    update

--- run_experiments.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python rag_experiment.py --k 5 --llm_name mistralai/Mistral-7B-Instruct-v0.2 --retriever_name MedCPT --corpus_name Textbooks
-
-# add more experiments as wanted
